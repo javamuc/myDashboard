cc534ba217b9d367de01ce2de14fc602cfc88674
Sancar <sancar.gazi@gmail.com>
2025-01-20 13:55:36 +0100

    adds a new clock component to the dashboard

---
+++ src/main/webapp/app/shared/clock/clock.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'jhi-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class ClockComponent implements OnInit, OnDestroy {
+  currentTime: Date = new Date();
+  private timer: any;
+
+  ngOnInit(): void {
+    // Update time immediately
+    this.updateTime();
+    // Set up timer to update every minute
+    this.timer = setInterval(() => this.updateTime(), 60000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  private updateTime(): void {
+    this.currentTime = new Date();
+  }
+}
